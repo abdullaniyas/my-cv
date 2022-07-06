fbf98a34ec72bbeb9e5e00b0327c7723835ded69
Abdulla Niyas <abdniya1@in.ibm.com>
2022-07-06 21:08:34 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      </header>
+      <div className='container'>
+        <aside>
+          about me, skills, languages, hobbies comes here
+        </aside>
+        <section>
+          education, experience etc comes here
+        </section>
+      </div>
+      <footer>
+
+      </footer>
+    </div>
+  );
+}
+
+export default App;
